39eee8f504f70faa55703c57ce08e869b9f20ddf
SLJ-DESIGNS <sljdesigns.skr@gmail.com>
2023-10-09 16:30:53 +0530

    Initialize project

---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,73 @@
+@file:Suppress("unused")
+
+object Versions {
+    // Plugins
+    const val gradle = "8.1.1"
+    const val kotlin = "1.9.10"
+    const val sonatype = "1.3.0"
+    const val ksp = "$kotlin-1.0.13"
+
+    // OneSignal
+    const val oneSignal = "4.8.6"
+
+    // App
+    const val minSdk = 21
+    const val targetSdk = 34
+    const val buildTools = "34.0.0"
+
+    // Coroutines
+    const val coroutines = "1.7.3"
+
+    // Android UI
+    const val appcompat = "1.6.1"
+    const val cardView = "1.0.0"
+    const val recyclerView = "1.3.1"
+    const val swipeRefreshLayout = "1.1.0"
+    const val palette = "1.0.0"
+    const val preference = "1.2.1"
+    const val materialComponents = "1.9.0"
+    const val activityKtx = "1.7.2"
+    const val fragmentKtx = "1.6.1"
+
+    // ViewModel and LiveData
+    const val lifecycle = "2.6.2"
+    const val lifecycleExt = "2.2.0"
+
+    // Room Database
+    const val room = "2.5.2"
+
+    // Work Manager
+    const val work = "2.8.1"
+
+    // Network & Serialization
+    const val gson = "2.10.1"
+    const val retrofit = "2.9.0"
+
+    // Image loading and Touch Image View
+    const val coil = "2.4.0"
+    const val touchImageView = "3.6"
+
+    // Harmonic Colors
+    const val harmonicColors = "1.0.0"
+
+    // Sectioned RecyclerView
+    const val sectionedRecyclerView = "ea9a5cb"
+
+    // FastScroll RecyclerView
+    const val fastScrollRecyclerView = "-SNAPSHOT"
+
+    // Permissions
+    const val permissions = "3.4.0"
+
+    // License Checker
+    const val licenseChecker = "1.2.8"
+
+    // In-App Billing
+    const val inAppBilling = "6.0.1"
+
+    // MultiDex
+    const val multidex = "2.0.1"
+
+    // Muzei
+    const val muzei = "3.4.1"
+}
